74fce8574f5a7ce7c199d168af2a3fed43fab07c
Andrew <agrablewski@gmail.com>
2020-09-26 08:34:19 -0400

    build login form

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'rails', '~> 5.2.4'
 gem 'puma', '~> 3.11'
 gem 'semantic-ui-sass'
 gem 'jquery-rails'
+gem 'hirb'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Andrew", password: "password")
+User.create(username: "Ringo", password: "password")
+User.create(username: "Jane", password: "password")
+User.create(username: "LadyG", password: "password")
